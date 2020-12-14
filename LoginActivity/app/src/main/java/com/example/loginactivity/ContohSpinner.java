e09493ec03b5b548c0428f078e7fbf5eac76bfd3
kalitrack18 <kalitrack354@gmail.com>
2020-12-14 22:16:04 +0800

    Membuat Spinner

---
+++ LoginActivity/app/src/main/java/com/example/loginactivity/ContohSpinner.java
@@ -0,0 +1,14 @@
+package com.example.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContohSpinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contoh_spinner);
+    }
+}
